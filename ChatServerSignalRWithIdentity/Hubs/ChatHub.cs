da3ab90b866b197814f0e7293485db052e6b5184
Professional <shapoval.ju@yandex.ru>
2020-06-11 16:58:51 +0300

    ProjectByVideo.Start

---
+++ ChatServerSignalRWithIdentity/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatServerSignalRWithIdentity.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatServerSignalRWithIdentity.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message) =>
+            await Clients.All.SendAsync("receiveMessage", message);
+
+          
+    }
+}
---
+++ ChatServerSignalRWithIdentity/Models/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ChatServerSignalRWithIdentity.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public virtual ICollection<Message> Messages { get; set; }
+
+        public AppUser()
+        {
+            Messages = new HashSet<Message>();
+        }
+    }
+}
